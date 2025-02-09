ec207ff7f613657243300818b369b2ed577e643c
r3d5un <r3d5un@pm.me>
2025-02-09 19:38:56 +0100

    WIP OpenTelemetry

---
+++ Go/internal/telemetry/telemetry.go
@@ -0,0 +1,69 @@
+package telemetry
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploggrpc"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/sdk/log"
+	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+)
+
+func NewLoggerProvider(ctx context.Context, res *resource.Resource) (*log.LoggerProvider, error) {
+	exporter, err := otlploggrpc.New(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create OLTP log exporter: %w", err)
+	}
+
+	processor := log.NewBatchProcessor(exporter)
+	return log.NewLoggerProvider(log.WithProcessor(processor), log.WithResource(res)), nil
+}
+
+func NewMeterProvider(ctx context.Context, res *resource.Resource) (*metric.MeterProvider, error) {
+	exporter, err := otlpmetricgrpc.New(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create OTLP metric exporter: %w", err)
+	}
+
+	mp := metric.NewMeterProvider(
+		metric.WithReader(metric.NewPeriodicReader(exporter)),
+		metric.WithResource(res),
+	)
+	otel.SetMeterProvider(mp)
+
+	return mp, nil
+}
+
+func NewTracerProvider(ctx context.Context, res *resource.Resource) (*trace.TracerProvider, error) {
+	exporter, err := otlptracegrpc.New(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create OTLP trace exporter: %w", err)
+	}
+
+	// Create Resource
+	tp := trace.NewTracerProvider(
+		trace.WithBatcher(exporter),
+		trace.WithResource(res),
+	)
+	otel.SetTracerProvider(tp)
+
+	return tp, nil
+}
+
+func NewResource(serviceName string, serviceVersion string) *resource.Resource {
+	hostName, _ := os.Hostname()
+
+	return resource.NewWithAttributes(
+		semconv.SchemaURL,
+		semconv.ServiceName(serviceName),
+		semconv.ServiceVersion(serviceVersion),
+		semconv.HostName(hostName),
+	)
+}
