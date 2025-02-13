9ae369fcba1f45d6536ffa7a3e4048b28df4da40
r3d5un <r3d5un@pm.me>
2025-02-13 20:59:36 +0100

    Initialize telemetry through SetupTelemetry

--- Go/cmd/api/main.go
+++ Go/cmd/api/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"log/slog"
 	_ "net/http/pprof"
 	"os"
+	"os/signal"
 
 	"github.com/google/uuid"
 	"github.com/r3d5un/rosetta/Go/internal/api"
+	"github.com/r3d5un/rosetta/Go/internal/telemetry"
 )
 
-const version = "0.0.1"
+const (
+	appName    = "rosetta"
+	appVersion = "0.0.1"
+)
 
 func main() {
 	if err := run(); err != nil {
@@ -21,15 +28,27 @@ func main() {
 }
 
 func run() error {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
 	handler := slog.NewJSONHandler(os.Stdout, nil)
 	logger := slog.New(handler).With(
 		slog.Group(
 			"applicationInstance",
-			slog.String("version", version),
+			slog.String("name", appName),
+			slog.String("version", appVersion),
 			slog.String("instanceId", uuid.New().String()),
 		),
 	)
 	slog.SetDefault(logger)
+
+	shutdownTelemetry, err := telemetry.SetupTelemetry(ctx, appName, appVersion)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		err = errors.Join(err, shutdownTelemetry(context.Background()))
+	}()
 
 	app := api.NewAPI(*logger)
 	if err := app.Serve(); err != nil {
@@ -38,4 +57,8 @@ func run() error {
 	}
 
 	return nil
+}
+
+func otelShutdown(context context.Context) {
+	panic("unimplemented")
 }
