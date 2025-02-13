75b2d4b2799a728a468bc276c25779c13b1e4222
r3d5un <r3d5un@pm.me>
2025-02-13 20:58:53 +0100

    Rework telemetry package

--- Go/internal/telemetry/telemetry.go
+++ Go/internal/telemetry/telemetry.go
@@ -2,13 +2,16 @@ package telemetry
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os"
 
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploggrpc"
-	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/exporters/stdout/stdoutlog"
+	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/log/global"
+	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/log"
 	"go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
@@ -16,8 +19,66 @@ import (
 	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 )
 
-func NewLoggerProvider(ctx context.Context, res *resource.Resource) (*log.LoggerProvider, error) {
-	exporter, err := otlploggrpc.New(ctx)
+func SetupTelemetry(
+	ctx context.Context,
+	serviceName string,
+	serviceVersion string,
+) (shutdown func(context.Context) error, err error) {
+	var shutdownFuncs []func(context.Context) error
+	shutdown = func(ctx context.Context) error {
+		var err error
+		for _, fn := range shutdownFuncs {
+			err = errors.Join(err, fn(ctx))
+		}
+		shutdownFuncs = nil
+		return err
+	}
+
+	handleErr := func(inErr error) {
+		err = errors.Join(inErr, shutdown(ctx))
+	}
+
+	resource := newResource(serviceName, serviceVersion)
+
+	propagator := newPropagator()
+	otel.SetTextMapPropagator(propagator)
+
+	traceProvider, err := newwTracerProvider(ctx, resource)
+	if err != nil {
+		handleErr(err)
+		return
+	}
+	shutdownFuncs = append(shutdownFuncs, traceProvider.Shutdown)
+	otel.SetTracerProvider(traceProvider)
+
+	meterProvider, err := newMeterProvider(ctx, resource)
+	if err != nil {
+		handleErr(err)
+		return
+	}
+	shutdownFuncs = append(shutdownFuncs, meterProvider.Shutdown)
+	otel.SetMeterProvider(meterProvider)
+
+	loggerProvider, err := newLoggerProvider(ctx, resource)
+	if err != nil {
+		handleErr(err)
+		return
+	}
+	shutdownFuncs = append(shutdownFuncs, loggerProvider.Shutdown)
+	global.SetLoggerProvider(loggerProvider)
+
+	return shutdown, nil
+}
+
+func newPropagator() propagation.TextMapPropagator {
+	return propagation.NewCompositeTextMapPropagator(
+		propagation.TraceContext{},
+		propagation.Baggage{},
+	)
+}
+
+func newLoggerProvider(ctx context.Context, res *resource.Resource) (*log.LoggerProvider, error) {
+	exporter, err := stdoutlog.New()
 	if err != nil {
 		return nil, fmt.Errorf("failed to create OLTP log exporter: %w", err)
 	}
@@ -26,8 +87,8 @@ func NewLoggerProvider(ctx context.Context, res *resource.Resource) (*log.Logger
 	return log.NewLoggerProvider(log.WithProcessor(processor), log.WithResource(res)), nil
 }
 
-func NewMeterProvider(ctx context.Context, res *resource.Resource) (*metric.MeterProvider, error) {
-	exporter, err := otlpmetricgrpc.New(ctx)
+func newMeterProvider(ctx context.Context, res *resource.Resource) (*metric.MeterProvider, error) {
+	exporter, err := stdoutmetric.New()
 	if err != nil {
 		return nil, fmt.Errorf("failed to create OTLP metric exporter: %w", err)
 	}
@@ -41,13 +102,15 @@ func NewMeterProvider(ctx context.Context, res *resource.Resource) (*metric.Mete
 	return mp, nil
 }
 
-func NewTracerProvider(ctx context.Context, res *resource.Resource) (*trace.TracerProvider, error) {
-	exporter, err := otlptracegrpc.New(ctx)
+func newwTracerProvider(
+	ctx context.Context,
+	res *resource.Resource,
+) (*trace.TracerProvider, error) {
+	exporter, err := stdouttrace.New()
 	if err != nil {
 		return nil, fmt.Errorf("failed to create OTLP trace exporter: %w", err)
 	}
 
-	// Create Resource
 	tp := trace.NewTracerProvider(
 		trace.WithBatcher(exporter),
 		trace.WithResource(res),
@@ -57,7 +120,7 @@ func NewTracerProvider(ctx context.Context, res *resource.Resource) (*trace.Trac
 	return tp, nil
 }
 
-func NewResource(serviceName string, serviceVersion string) *resource.Resource {
+func newResource(serviceName string, serviceVersion string) *resource.Resource {
 	hostName, _ := os.Hostname()
 
 	return resource.NewWithAttributes(
