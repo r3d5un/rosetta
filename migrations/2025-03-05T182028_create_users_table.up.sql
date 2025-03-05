5bb30c0d1af40aa672961ff6add0e79b3b5ebec6
r3d5un <r3d5un@pm.me>
2025-03-05 19:31:37 +0100

    Create table for forum users

---
+++ migrations/2025-03-05T182028_create_users_table.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS forum.users
+(
+    id         UUID      DEFAULT gen_random_uuid(),
+    name       VARCHAR(256)            NOT NULL,
+    username   VARCHAR(256)            NOT NULL,
+    email      VARCHAR(256)            NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW() NOT NULL,
+    updated_at TIMESTAMP DEFAULT NOW() NOT NULL,
+    CONSTRAINT primary_key PRIMARY KEY (id),
+    CONSTRAINT email_unique_constraint UNIQUE (email),
+    CONSTRAINT username_unique_constraint UNIQUE (username)
+);
