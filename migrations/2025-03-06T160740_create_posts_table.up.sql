b6ea38f61a62629ce3ab3ee9f65f30e66b7dab5e
r3d5un <r3d5un@pm.me>
2025-03-06 17:52:18 +0100

    Add reply_to column and a self referential foreign key to other posts

--- migrations/2025-03-06T160740_create_posts_table.up.sql
+++ migrations/2025-03-06T160740_create_posts_table.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS forum.posts
 (
     id         UUID      DEFAULT gen_random_uuid(),
     thread_id  UUID                    NOT NULL,
+    reply_to   UUID                    NULL,
     author_id  UUID                    NOT NULL,
     content    TEXT                    NOT NULL,
     created_at TIMESTAMP DEFAULT NOW() NOT NULL,
@@ -9,5 +10,6 @@ CREATE TABLE IF NOT EXISTS forum.posts
     likes      INTEGER   DEFAULT 0     NOT NULL,
     CONSTRAINT pk_posts PRIMARY KEY (id),
     CONSTRAINT fk_thread_id FOREIGN KEY (thread_id) REFERENCES forum.threads (id),
-    CONSTRAINT fk_author_id FOREIGN KEY (author_id) REFERENCES forum.users (id)
+    CONSTRAINT fk_author_id FOREIGN KEY (author_id) REFERENCES forum.users (id),
+    CONSTRAINT srfk_replies_post_id FOREIGN KEY (reply_to) REFERENCES forum.posts (id)
 );
