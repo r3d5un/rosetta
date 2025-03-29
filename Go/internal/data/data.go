c663c56e97a6770877821d2749b65994254f3d41
r3d5un <r3d5un@pm.me>
2025-03-29 14:39:32 +0100

    Add enums for PostgreSQL error codes

--- Go/internal/data/data.go
+++ Go/internal/data/data.go
@@ -4,6 +4,27 @@ import (
 	"strings"
 )
 
+const (
+	// PgxIntegrityConstraintViolationCode is the code for general integrity violations
+	PgxIntegrityConstraintViolationCode string = "23000"
+	// PgxRestrictViolationCode is the error code for deleting or updating records referenced by other resources
+	PgxRestrictViolationCode = "23001"
+	// PgxNotNullViolationCode is the error code for attempting to insert or update NULL values in a NOT NULL column
+	PgxNotNullViolationCode = "23502"
+	// PgxForeignKeyViolationCode is for foreign key violation, e.g. creating values not in the referenced table
+	PgxForeignKeyViolationCode = "23503"
+	// PgxUniqueViolationCode is the error code for inserting duplicate records
+	PgxUniqueViolationCode = "23505"
+	// PgxCheckViolationCode is for CHECK violations
+	PgxCheckViolationCode = "23514"
+	// PgxSyntaxErrorCode is for general syntax errors
+	PgxSyntaxErrorCode = "42601"
+	// PgxUndefinedColumnCode is the error code for referencing columns that does not exists
+	PgxUndefinedColumnCode = "42703"
+	// PgxUndefinedTableCode is the error code for refercing tables that does not exists
+	PgxUndefinedTableCode = "42P01"
+)
+
 // CreateOrderByClause creates a SQL statement for ordering based on a given
 // list of column names. Column names prefixed in "-" will be in a descending
 // order.
