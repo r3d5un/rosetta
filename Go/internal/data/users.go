8e6f69af0df6b70476d3072a712f2c1d2e0c7d09
r3d5un <r3d5un@pm.me>
2025-03-29 13:13:02 +0100

    Add a SelectAll method to the UserModel

--- Go/internal/data/users.go
+++ Go/internal/data/users.go
@@ -70,3 +70,82 @@ WHERE id = $1;
 
 	return &u, nil
 }
+
+func (m *UserModel) SelectAll(ctx context.Context, filters Filters) ([]*User, *Metadata, error) {
+	query := `
+SELECT id, name, username, email, created_at, updated_at
+FROM forum.users
+WHERE ($2 IS NULL OR id = $2)
+  AND ($3 IS NULL or name = $3)
+  AND ($4 IS NULL or username = $4)
+  AND ($5 IS NULL or email = $5)
+  AND ($6 IS NULL or created_at >= $6)
+  AND ($7 IS NULL or created_at <= $7)
+  AND ($8 IS NULL or updated_at >= $8)
+  AND ($9 IS NULL or updated_at <= $9)
+` + CreateOrderByClause(filters.OrderBy) + `
+LIMIT $1
+`
+
+	ctx, cancel := context.WithTimeout(ctx, *m.Timeout)
+	defer cancel()
+
+	logger := logging.LoggerFromContext(ctx).With(slog.Group(
+		"query",
+		slog.String("statement", logging.MinifySQL(query)),
+		slog.Any("filters", filters),
+	))
+
+	logger.Info("performing query")
+	rows, err := m.DB.Query(
+		ctx,
+		query,
+		filters.PageSize,
+		filters.ID,
+		filters.Name,
+		filters.Username,
+		filters.Email,
+		filters.CreatedAtFrom,
+		filters.CreatedAtTo,
+		filters.UpdatedAtFrom,
+		filters.UpdatedAtTo,
+	)
+	if err != nil {
+		logger.Error("unable to perform query", slog.String("error", err.Error()))
+		return nil, nil, err
+	}
+
+	users := []*User{}
+
+	for rows.Next() {
+		var user User
+
+		err := rows.Scan(
+			&user.ID,
+			&user.Name,
+			&user.Username,
+			&user.Email,
+			&user.CreatedAt,
+			&user.UpdatedAt,
+		)
+		if err != nil {
+			logger.Error("unable to scan query result", slog.String("error", err.Error()))
+			return nil, nil, err
+		}
+		users = append(users, &user)
+	}
+	if err = rows.Err(); err != nil {
+		logger.Error("unable to scan query result", slog.String("error", err.Error()))
+		return nil, nil, err
+	}
+	length := len(users)
+	var metadata Metadata
+	if length > 0 {
+		metadata.LastSeen = users[length-1].ID
+		metadata.Next = true
+	}
+	metadata.ResponseLength = length
+
+	logger.Info("query returned users", slog.Any("metadata", metadata))
+	return users, &metadata, nil
+}
