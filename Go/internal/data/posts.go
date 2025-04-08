d5338516173662242b3aeae6e8237bb3869101da
r3d5un <r3d5un@pm.me>
2025-04-08 20:29:00 +0200

    Create field documentation for the PostModel

--- Go/internal/data/posts.go
+++ Go/internal/data/posts.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"database/sql"
 	"time"
 
 	"github.com/google/uuid"
@@ -8,14 +9,37 @@ import (
 )
 
 type Post struct {
-	ID        uuid.UUID  `json:"id"`
-	ThreadID  uuid.UUID  `json:"threadId"`
-	ReplyTo   *uuid.UUID `json:"replyTo"`
-	AuthorID  uuid.UUID  `json:"authorId"`
-	Content   string     `json:"content"`
-	CreatedAt time.Time  `json:"createdAt"`
-	UpdatedAt time.Time  `json:"updatedAt"`
-	Likes     int64      `json:"likes"`
+	// ID is the unique identifier of the thread
+	//
+	// Upon creating a new thread, any existing values in this field is ignored. The database handles
+	// setting the value upon insertion.
+	ID uuid.UUID `json:"id"`
+	// ThreadID is the ID of the parent thread.
+	ThreadID uuid.UUID `json:"threadId"`
+	// ReplyTo is the ID of which this post is a reply to.
+	ReplyTo *uuid.UUID `json:"replyTo"`
+	// AuthorID is the unique identifier of the author of the thread.
+	AuthorID uuid.UUID `json:"authorId"`
+	// Context is the actual text content of a post
+	Content string `json:"content"`
+	// CreatedAt denotes when a thread was created.
+	//
+	// Upon creating a new thread, any existing values in this field is ignored. The database handles
+	// setting the value upon insertion.
+	CreatedAt time.Time `json:"createdAt"`
+	// UpdatedAt denotes when a thread was last updated.
+	//
+	// Upon creating a new thread, any existing values in this field is ignored. The database handles
+	// setting the value upon insertion.
+	UpdatedAt time.Time `json:"updatedAt"`
+	// Likes is the total number of votes a post has received
+	Likes int64 `json:"likes"`
+	// Deleted is a soft delete flag for a thread.
+	Deleted bool `json:"deleted,omitzero"`
+	// DeletedAt denotes when a thread was marked as deleted.
+	//
+	// This field is ignored when updating or creating new thread.
+	DeletedAt sql.NullTime `json:"deletedAt,omitzero"`
 }
 
 type PostModel struct {
