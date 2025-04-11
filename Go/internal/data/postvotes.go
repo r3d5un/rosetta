14bbda477050bda7501a439c31cac0cd390782d2
r3d5un <r3d5un@pm.me>
2025-04-11 23:12:58 +0200

    Rewrite queries for the PostVoteModel methods

--- Go/internal/data/postvotes.go
+++ Go/internal/data/postvotes.go
@@ -27,14 +27,17 @@ type PostVoteModel struct {
 
 func (m *PostVoteModel) SelectCount(ctx context.Context, filters Filters) (*int, error) {
 	const query string = `
-SELECT COUNT(*)
+SELECT CASE
+           WHEN SUM(vote) IS NULL THEN 0
+           ELSE SUM(vote)
+           END AS total_votes
 FROM forum.post_votes
-WHERE ($2::UUID IS NULL OR post_id = $2::UUID)
-  AND ($3::UUID IS NULL OR user_id = $2::UUID);
+WHERE ($1::UUID IS NULL OR post_id = $1::UUID)
+  AND ($2::UUID IS NULL OR user_id = $2::UUID);
 `
 	logger := logging.LoggerFromContext(ctx).With(slog.Group(
 		"query",
-		slog.String("query", query),
+		slog.String("query", logging.MinifySQL(query)),
 		slog.Any("filters", filters),
 		slog.Duration("timeout", *m.Timeout),
 	))
@@ -42,6 +45,8 @@ WHERE ($2::UUID IS NULL OR post_id = $2::UUID)
 	var count int
 
 	logger.Info("performing query")
+	ctx, cancel := context.WithTimeout(ctx, *m.Timeout)
+	defer cancel()
 	err := m.DB.QueryRow(ctx, query, filters.PostID, filters.UserID).Scan(&count)
 	if err != nil {
 		return nil, handleError(err, logger)
@@ -49,4 +54,44 @@ WHERE ($2::UUID IS NULL OR post_id = $2::UUID)
 	logger.Info("count complete", slog.Int("count", count))
 
 	return &count, nil
+}
+
+// Vote performs a upsert for to record any votes for any post. If the vote is 0, the record is
+// deleted.
+func (m *PostVoteModel) Vote(ctx context.Context, vote PostVote) (*PostVote, error) {
+	const query string = `
+WITH input_data AS (SELECT $1::UUID     AS post_id,
+                           $2::UUID     AS user_id,
+                           $3::SMALLINT AS vote),
+     delete_if_zero AS (
+         DELETE FROM forum.post_votes
+             WHERE post_id = (SELECT post_id FROM input_data)
+                 AND user_id = (SELECT user_id FROM input_data)
+                 AND (SELECT vote FROM input_data) = 0)
+INSERT
+INTO forum.post_votes (post_id, user_id, vote)
+SELECT post_id, user_id, vote
+FROM input_data
+WHERE vote != 0
+ON CONFLICT (post_id, user_id) DO UPDATE
+    SET vote = EXCLUDED.vote;
+`
+
+	logger := logging.LoggerFromContext(ctx).With(slog.Group(
+		"query",
+		slog.String("query", logging.MinifySQL(query)),
+		slog.Any("vote", vote),
+		slog.Duration("timeout", *m.Timeout),
+	))
+
+	logger.Info("performing query")
+	ctx, cancel := context.WithTimeout(ctx, *m.Timeout)
+	defer cancel()
+	result, err := m.DB.Exec(ctx, query, vote.PostID, vote.UserID, vote.Vote)
+	if err != nil {
+		return nil, handleError(err, logger)
+	}
+	logger.Info("query performed", slog.Any("affectedRows", result.RowsAffected()))
+
+	return &vote, nil
 }
