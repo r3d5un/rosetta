27757ffeda626eca3d16ed00701329dd4c7248af
r3d5un <r3d5un@pm.me>
2025-04-12 00:04:53 +0200

    Create SelectAll method for the PostModel

--- Go/internal/data/posts.go
+++ Go/internal/data/posts.go
@@ -101,3 +101,99 @@ WHERE id = $1::UUID;
 
 	return &p, nil
 }
+
+func (m *PostModel) SelectAll(ctx context.Context, filters Filters) ([]*Post, *Metadata, error) {
+	query := `
+SELECT id,
+       thread_id,
+       reply_to,
+       author_id,
+       content,
+       created_at,
+       updated_at,
+       likes,
+       deleted,
+       deleted_at
+FROM forum.posts
+WHERE ($2::UUID IS NULL OR id = $2::UUID)
+  AND ($3::UUID IS NULL OR thread_id = $3::UUID)
+  AND ($4::VARCHAR(256) IS NULL OR title = $4::VARCHAR(256))
+  AND ($5::UUID IS NULL OR author_id = $5::UUID)
+  AND ($6::TIMESTAMP IS NULL or created_at >= $6::TIMESTAMP)
+  AND ($7::TIMESTAMP IS NULL or created_at <= $7::TIMESTAMP)
+  AND ($8::TIMESTAMP IS NULL or updated_at >= $8::TIMESTAMP)
+  AND ($9::TIMESTAMP IS NULL or updated_at <= $9::TIMESTAMP)
+  AND ($10::BOOLEAN IS NULL or deleted = $10::BOOLEAN)
+  AND ($11::TIMESTAMP IS NULL or deleted_at >= $11::TIMESTAMP)
+  AND ($12::TIMESTAMP IS NULL or deleted_at <= $12::TIMESTAMP)
+  AND id > $14::UUID
+` + CreateOrderByClause(filters.OrderBy) + `
+LIMIT $1::INTEGER;
+`
+
+	logger := logging.LoggerFromContext(ctx).With(slog.Group(
+		"query",
+		slog.String("query", query),
+		slog.Any("filters", filters),
+		slog.Duration("timeout", *m.Timeout),
+	))
+
+	logger.Info("performing query")
+	rows, err := m.DB.Query(
+		ctx,
+		query,
+		filters.PageSize,
+		filters.ID,
+		filters.ThreadID,
+		filters.Title,
+		filters.AuthorID,
+		filters.CreatedAtFrom,
+		filters.CreatedAtTo,
+		filters.UpdatedAtFrom,
+		filters.UpdatedAtTo,
+		filters.Deleted,
+		filters.DeletedAtFrom,
+		filters.DeletedAtTo,
+		filters.LastSeen,
+	)
+	if err != nil {
+		logger.Error("unable to perform query", slog.String("error", err.Error()))
+		return nil, nil, err
+	}
+
+	posts := []*Post{}
+
+	for rows.Next() {
+		var p Post
+
+		err := rows.Scan(
+			&p.ID,
+			&p.ThreadID,
+			&p.ReplyTo,
+			&p.AuthorID,
+			&p.Content,
+			&p.CreatedAt,
+			&p.UpdatedAt,
+			&p.Likes,
+			&p.Deleted,
+			&p.DeletedAt,
+		)
+		if err != nil {
+			return nil, nil, handleError(err, logger)
+		}
+		posts = append(posts, &p)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, nil, handleError(err, logger)
+	}
+	length := len(posts)
+	var metadata Metadata
+	if length > 0 {
+		metadata.LastSeen = posts[length-1].ID
+		metadata.Next = true
+	}
+	metadata.ResponseLength = length
+
+	logger.Info("posts selected", slog.Any("metadata", metadata))
+	return posts, &metadata, nil
+}
