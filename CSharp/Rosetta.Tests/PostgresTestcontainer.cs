50e98ddccd655f50d4caf3235eb91f1312ef2d97
r3d5un <r3d5un@pm.me>
2025-04-12 21:17:17 +0200

    WIP PostgreSQL Testcontainer for the C#/.NET project

---
+++ CSharp/Rosetta.Tests/PostgresTestcontainer.cs
@@ -0,0 +1,59 @@
+using DotNet.Testcontainers.Containers;
+using Npgsql;
+using System;
+using System.Data;
+using System.Data.Common;
+using System.Threading.Tasks;
+using Testcontainers.PostgreSql;
+using Xunit;
+
+namespace Rosetta.Tests;
+
+public class PostgresTestcontainer : IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _postgreSqlContainer = new PostgreSqlBuilder()
+        .WithDatabase("postgres")
+        .WithUsername("postgres")
+        .WithPassword("postgres")
+        .Build();
+    
+    public async Task InitializeAsync()
+    {
+        await _postgreSqlContainer.StartAsync();
+    }
+
+    public Task DisposeAsync()
+    {
+        return _postgreSqlContainer.DisposeAsync().AsTask();
+    }
+
+    public string GetConnectionString() => _postgreSqlContainer.GetConnectionString();
+    
+    [Fact]
+    public void ConnectionStateReturnsOpen()
+    {
+        // Given
+        using DbConnection connection = new NpgsqlConnection(_postgreSqlContainer.GetConnectionString());
+
+        // When
+        connection.Open();
+
+        // Then
+        Assert.Equal(ConnectionState.Open, connection.State);
+    }
+
+    [Fact]
+    public async Task ExecScriptReturnsSuccessful()
+    {
+        // Given
+        const string scriptContent = "SELECT 1;";
+
+        // When
+        var execResult = await _postgreSqlContainer.ExecScriptAsync(scriptContent)
+            .ConfigureAwait(true);
+
+        // Then
+        Assert.True(0L.Equals(execResult.ExitCode), execResult.Stderr);
+        Assert.Empty(execResult.Stderr);
+    }
+}
