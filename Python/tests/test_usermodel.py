27f4c0455f540ef0b5c2b7a89c0e7f556b904fbc
r3d5un <r3d5un@pm.me>
2025-04-16 23:04:43 +0200

    Test the UserModel insert method

--- Python/tests/test_usermodel.py
+++ Python/tests/test_usermodel.py
@@ -1,4 +1,17 @@
-from tests.conftest import get_connection
+from python.db.user import User, UserModel
+from tests.conftest import get_connection, get_testcontainer_db_engine
+
+test_user = User(
+    name="Johnny Silverhand", username="samurai", email="jsilverhand@samurai.com"
+)
+
+
+def test_insert():
+    user_model = UserModel(get_testcontainer_db_engine())
+    print(f"inserting user: {test_user}")
+    inserted_user = user_model.insert(test_user)
+    print(f"inserted user: {inserted_user}")
+    assert inserted_user is not None
 
 
 def test_something():
