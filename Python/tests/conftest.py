472e4f618c66f1d8e69f39532e3b9b2e8fb87fd7
r3d5un <r3d5un@pm.me>
2025-04-16 22:11:04 +0200

    Run down migration in pytest fixtures

--- Python/tests/conftest.py
+++ Python/tests/conftest.py
@@ -3,6 +3,8 @@ import typing
 
 import psycopg
 import pytest
+from sqlalchemy import Engine
+from sqlmodel import create_engine
 from testcontainers.postgres import PostgresContainer
 
 postgres = PostgresContainer("postgres:17.4")
@@ -26,29 +28,7 @@ def setup_database(request):
 
 @pytest.fixture(scope="function")
 def db_connection():
-    host = postgres.get_container_host_ip()
-    port = str(postgres.get_exposed_port(5432))
-    username = postgres.username
-    password = postgres.password
-    database = postgres.dbname
-
-    return psycopg.connect(
-        f"host={host} dbname={database} user={username} password={password} port={port}"
-    )
-
-
-@pytest.fixture(scope="function", autouse=True)
-def apply_migrations(db_connection):
-    migrations_dir = f"{find_project_root()}/migrations"
-    migrations = list_up_migrations(migrations_dir)
-    for migration in migrations:
-        print(f"Performing up migration: {migration}")
-        with open(migration) as f:
-            migration_sql = f.read()
-            with db_connection.cursor() as c:
-                c.execute(typing.cast(typing.LiteralString, migration_sql))
-                db_connection.commit()
-    print("Database migration complete")
+    return get_connection()
 
 
 def get_connection() -> psycopg.Connection:
@@ -63,6 +43,44 @@ def get_connection() -> psycopg.Connection:
     )
 
 
+@pytest.fixture(scope="function", autouse=True)
+def apply_migrations(db_connection):
+    migrations_dir = f"{find_project_root()}/migrations"
+    up_migrations = list_up_migrations(migrations_dir)
+    for migration in up_migrations:
+        print(f"Performing up migration: {migration}")
+        with open(migration) as f:
+            migration_sql = f.read()
+            with db_connection.cursor() as c:
+                c.execute(typing.cast(typing.LiteralString, migration_sql))
+                db_connection.commit()
+    print("Database migration complete")
+
+    yield
+
+    down_migrations = list_down_migrations(migrations_dir)
+    for migration in down_migrations:
+        print(f"Performing down migration: {migration}")
+        with open(migration) as f:
+            migration_sql = f.read()
+            with db_connection.cursor() as c:
+                c.execute(typing.cast(typing.LiteralString, migration_sql))
+                db_connection.commit()
+    print("Database down migration complete")
+
+
+def get_testcontainer_db_engine() -> Engine:
+    host = postgres.get_container_host_ip()
+    port = str(postgres.get_exposed_port(5432))
+    username = postgres.username
+    password = postgres.password
+    database = postgres.dbname
+    return create_engine(
+        f"postgresql://{username}:{password}@{host}:{port}/{database}?sslmod=disable",
+        echo=True,
+    )
+
+
 def list_up_migrations(dir_path: str) -> list[str]:
     migrations = []
     try:
@@ -73,6 +91,23 @@ def list_up_migrations(dir_path: str) -> list[str]:
     for file in files:
         if not os.path.isdir(os.path.join(dir_path, file)) and file.endswith(".up.sql"):
             migrations.append(os.path.join(dir_path, file))
+
+    return migrations
+
+
+def list_down_migrations(dir_path: str) -> list[str]:
+    migrations = []
+    try:
+        files = os.listdir(dir_path)
+    except OSError as e:
+        raise e
+
+    for file in files:
+        if not os.path.isdir(os.path.join(dir_path, file)) and file.endswith(
+            ".down.sql"
+        ):
+            migrations.insert(0, os.path.join(dir_path, file))
+            migrations.sort(reverse=True)
 
     return migrations
 
