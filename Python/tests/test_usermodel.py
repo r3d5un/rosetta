fd83d0b1c251199b7129f759acec302fe37a3cbf
r3d5un <r3d5un@pm.me>
2025-04-17 13:06:48 +0200

    Add tests for the insert and select methods for the UserModel

--- Python/tests/test_usermodel.py
+++ Python/tests/test_usermodel.py
@@ -8,10 +8,37 @@ test_user = User(
 
 def test_insert():
     user_model = UserModel(get_testcontainer_db_engine())
-    print(f"inserting user: {test_user}")
-    inserted_user = user_model.insert(test_user)
-    print(f"inserted user: {inserted_user}")
-    assert inserted_user is not None
+
+    try:
+        inserted_user = user_model.insert(test_user)
+    except Exception as e:
+        raise Exception(f"error upon inserting user: {e}")
+    if inserted_user is None:
+        raise ValueError("no user returnd upon insertion")
+    if inserted_user.id is None:
+        raise ValueError("inserted user ID is None")
+
+    print(inserted_user)
+
+
+def test_select():
+    user_model = UserModel(get_testcontainer_db_engine())
+
+    try:
+        inserted_user = user_model.insert(test_user)
+    except Exception as e:
+        raise Exception(f"error upon inserting user: {e}")
+    if inserted_user is None:
+        raise ValueError("no user returnd upon insertion")
+    if inserted_user.id is None:
+        raise ValueError("inserted user ID is None")
+
+    selected_user = user_model.select(inserted_user.id)
+    if selected_user is None:
+        raise ValueError("no user returnd upon insertion")
+    assert selected_user.name == inserted_user.name
+    assert selected_user.username == inserted_user.username
+    assert selected_user.email == inserted_user.email
 
 
 def test_something():
