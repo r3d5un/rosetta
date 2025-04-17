b6661326310ada153163177c4cccda87b6e27180
r3d5un <r3d5un@pm.me>
2025-04-17 15:00:19 +0200

    Refactor UserModel methods

--- Python/src/python/db/user.py
+++ Python/src/python/db/user.py
@@ -52,26 +52,20 @@ class UserModel:
         session = sessionmaker(bind=self.engine)()
         with session:
             try:
-                results = session.execute(query, {"id": id})
-                session.commit()
-                users = [
-                    User(
-                        id=row.id,
-                        name=row.name,
-                        username=row.username,
-                        email=row.email,
-                        created_at=row.created_at,
-                        updated_at=row.updated_at,
-                        deleted=row.deleted,
-                        deleted_at=row.deleted_at,
-                    )
-                    for row in results
-                ]
-
-                if len(users) < 1:
+                row = session.execute(query, {"id": id}).first()
+                if row is None:
                     raise NoResultFound
-
-                return users[0]
+                session.commit()
+                return User(
+                    id=row.id,
+                    name=row.name,
+                    username=row.username,
+                    email=row.email,
+                    created_at=row.created_at,
+                    updated_at=row.updated_at,
+                    deleted=row.deleted,
+                    deleted_at=row.deleted_at,
+                )
             except Exception as e:
                 raise e
 
@@ -87,29 +81,23 @@ class UserModel:
         session = sessionmaker(bind=self.engine)()
         with session:
             try:
-                results = session.execute(
+                row = session.execute(
                     query,
                     {"name": user.name, "username": user.username, "email": user.email},
-                )
-                session.commit()
-                users = [
-                    User(
-                        id=row.id,
-                        name=row.name,
-                        username=row.username,
-                        email=row.email,
-                        created_at=row.created_at,
-                        updated_at=row.updated_at,
-                        deleted=row.deleted,
-                        deleted_at=row.deleted_at,
-                    )
-                    for row in results
-                ]
-
-                if len(users) < 1:
+                ).first()
+                if row is None:
                     raise NoResultFound
-
-                return users[0]
+                session.commit()
+                return User(
+                    id=row.id,
+                    name=row.name,
+                    username=row.username,
+                    email=row.email,
+                    created_at=row.created_at,
+                    updated_at=row.updated_at,
+                    deleted=row.deleted,
+                    deleted_at=row.deleted_at,
+                )
             except Exception as e:
                 raise e
 
@@ -129,37 +117,26 @@ class UserModel:
         session = sessionmaker(bind=self.engine)()
         with session:
             try:
-                parameters = {
-                    "id": user_patch.id,
-                    "name": (user_patch.name if user_patch.name is not None else None),
-                    "username": (
-                        user_patch.username if user_patch.username is not None else None
-                    ),
-                    "email": (
-                        user_patch.email if user_patch.email is not None else None
-                    ),
-                }
-                results = session.execute(
+                row = session.execute(
                     query,
-                    parameters,
-                )
+                    {
+                        "id": user_patch.id,
+                        "name": user_patch.name,
+                        "username": user_patch.username,
+                        "email": user_patch.email,
+                    },
+                ).first()
                 session.commit()
-                users = [
-                    User(
-                        id=row.id,
-                        name=row.name,
-                        username=row.username,
-                        email=row.email,
-                        created_at=row.created_at,
-                        updated_at=row.updated_at,
-                    )
-                    for row in results
-                ]
-
-                if len(users) < 1:
+                if row is None:
                     raise NoResultFound
-
-                return users[0]
+                return User(
+                    id=row.id,
+                    name=row.name,
+                    username=row.username,
+                    email=row.email,
+                    created_at=row.created_at,
+                    updated_at=row.updated_at,
+                )
             except Exception as e:
                 raise e
 
