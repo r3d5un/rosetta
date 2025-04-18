4251bd6a707cf226bb0ee66ade1d7ef92001b03a
r3d5un <r3d5un@pm.me>
2025-04-18 12:18:24 +0200

    Create test for the ForumModel select method

--- Python/tests/test_forummodel.py
+++ Python/tests/test_forummodel.py
@@ -27,3 +27,33 @@ def test_insert():
         raise ValueError("no forum returnd upon insertion")
     if inserted_forum.id is None:
         raise ValueError("inserted forum ID is None")
+
+
+def test_select():
+    models = Models(engine=get_testcontainer_db_engine())
+    test_user = User(
+        name="Saburo Arasaka", username="s.arasaka", email="s.arasaka@arasaka.com"
+    )
+    try:
+        user = models.users.insert(test_user)
+    except Exception as e:
+        raise Exception(f"error upon inserting user: {e}")
+    if user is None:
+        raise ValueError("no user returnd upon insertion")
+    if user.id is None:
+        raise ValueError("inserted user ID is None")
+
+    forum = Forum(owner_id=user.id, name="Crushing Militech", description="")
+    try:
+        inserted_forum = models.forums.insert(forum)
+    except Exception as e:
+        raise Exception(f"error upon inserting forum: {e}")
+    if inserted_forum is None:
+        raise ValueError("no forum returnd upon insertion")
+    if inserted_forum.id is None:
+        raise ValueError("inserted forum ID is None")
+
+    selected_forum = models.forums.select(inserted_forum.id)
+    if selected_forum is None:
+        raise ValueError("no user returnd upon insertion")
+    assert selected_forum.name == forum.name
