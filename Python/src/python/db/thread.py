20ef55a50e55672c4b04e2d2e326b26f9deeeaf1
r3d5un <r3d5un@pm.me>
2025-04-19 12:47:08 +0200

    Implement a ThreadModel insert method

--- Python/src/python/db/thread.py
+++ Python/src/python/db/thread.py
@@ -2,7 +2,9 @@ import uuid
 from datetime import datetime
 from typing import Optional
 
-from sqlalchemy import Engine
+from sqlalchemy import Engine, text
+from sqlalchemy.exc import NoResultFound
+from sqlalchemy.orm import sessionmaker
 from sqlmodel import Field, SQLModel
 
 
@@ -13,7 +15,7 @@ class Thread(SQLModel, table=True):
     id: Optional[uuid.UUID] = Field(default_factory=uuid.uuid4, primary_key=True)
     forum_id: uuid.UUID = Field(nullable=False)
     title: str = Field(nullable=False, max_length=256)
-    forum_id: uuid.UUID = Field(nullable=False)
+    author_id: uuid.UUID = Field(nullable=False)
     created_at: datetime = Field(nullable=False, default_factory=datetime.now)
     updated_at: datetime = Field(nullable=False, default_factory=datetime.now)
     is_locked: bool = Field(default=False, nullable=False)
@@ -35,3 +37,41 @@ class ThreadPatch(SQLModel):
 class ThreadModel:
     def __init__(self, engine: Engine):
         self.engine = engine
+
+    def insert(self, thread: Thread):
+        query = text(
+            """
+            INSERT INTO forum.threads(forum_id, title, author_id)
+            VALUES(:forum_id, :title, :author_id)
+            RETURNING id, forum_id, title, author_id, created_at, updated_at, is_locked, deleted, deleted_at, likes;
+            """
+        )
+
+        session = sessionmaker(bind=self.engine)()
+        with session:
+            try:
+                row = session.execute(
+                    query,
+                    {
+                        "forum_id": thread.forum_id,
+                        "title": thread.title,
+                        "author_id": thread.author_id,
+                    },
+                ).first()
+                if row is None:
+                    raise NoResultFound
+                session.commit()
+                return Thread(
+                    id=row.id,
+                    forum_id=row.forum_id,
+                    title=row.title,
+                    author_id=row.author_id,
+                    created_at=row.created_at,
+                    updated_at=row.updated_at,
+                    is_locked=row.is_locked,
+                    deleted=row.deleted,
+                    deleted_at=row.deleted_at,
+                    likes=row.likes,
+                )
+            except Exception as e:
+                raise e
---
+++ Python/tests/test_threadmodel.py
@@ -0,0 +1,40 @@
+from python.db.forum import Forum
+from python.db.session import Models
+from python.db.thread import Thread
+from python.db.user import User
+from tests.conftest import get_testcontainer_db_engine
+
+
+def test_insert():
+    models = Models(engine=get_testcontainer_db_engine())
+    user = User(
+        name="Saburo Arasaka", username="s.arasaka", email="s.arasaka@arasaka.com"
+    )
+    try:
+        user = models.users.insert(user)
+    except Exception as e:
+        raise Exception(f"error upon inserting user: {e}")
+    if user is None:
+        raise ValueError("no user returnd upon insertion")
+    if user.id is None:
+        raise ValueError("inserted user ID is None")
+
+    forum = Forum(owner_id=user.id, name="Crushing Militech", description="")
+    try:
+        inserted_forum = models.forums.insert(forum)
+    except Exception as e:
+        raise Exception(f"error upon inserting forum: {e}")
+    if inserted_forum is None:
+        raise ValueError("no forum returnd upon insertion")
+    if inserted_forum.id is None:
+        raise ValueError("inserted forum ID is None")
+
+    thread = Thread(forum_id=inserted_forum.id, author_id=user.id, title="Johnny Boy")
+    try:
+        thread = models.forums.insert(forum)
+    except Exception as e:
+        raise Exception(f"error upon inserting thread: {e}")
+    if thread is None:
+        raise ValueError("no thread returned upon insertion")
+    if thread.id is None:
+        raise ValueError("inserted thread ID is None")
