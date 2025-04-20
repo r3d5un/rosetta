766b98fc1b1b76926984b79a7fafdf00eb3369aa
r3d5un <r3d5un@pm.me>
2025-04-20 12:41:28 +0200

    Implement a PostModel restore method

--- Python/tests/test_postmodel.py
+++ Python/tests/test_postmodel.py
@@ -276,9 +276,66 @@ def test_soft_delete():
     try:
         post = models.posts.soft_delete(post.id)
     except Exception as e:
-        raise Exception(f"error upon inserting post: {e}")
+        raise Exception(f"error upon soft deleting post: {e}")
     if post is None:
-        raise ValueError("no post returned upon insertion")
+        raise ValueError("no post returned upon soft deletion")
     if post.deleted is None:
         raise ValueError("inserted post deleted status is None")
     assert post.deleted is True
+
+
+def test_restore():
+    models = Models(engine=get_testcontainer_db_engine())
+    user = User(
+        name="Saburo Arasaka", username="s.arasaka", email="s.arasaka@arasaka.com"
+    )
+    try:
+        user = models.users.insert(user)
+    except Exception as e:
+        raise Exception(f"error upon inserting user: {e}")
+    if user is None:
+        raise ValueError("no user returnd upon insertion")
+    if user.id is None:
+        raise ValueError("inserted user ID is None")
+
+    forum = Forum(owner_id=user.id, name="Crushing Militech", description="")
+    try:
+        inserted_forum = models.forums.insert(forum)
+    except Exception as e:
+        raise Exception(f"error upon inserting forum: {e}")
+    if inserted_forum is None:
+        raise ValueError("no forum returnd upon insertion")
+    if inserted_forum.id is None:
+        raise ValueError("inserted forum ID is None")
+
+    thread = Thread(forum_id=inserted_forum.id, author_id=user.id, title="Johnny Boy")
+    try:
+        thread = models.threads.insert(thread)
+    except Exception as e:
+        raise Exception(f"error upon inserting thread: {e}")
+    if thread is None:
+        raise ValueError("no thread returned upon insertion")
+    if thread.id is None:
+        raise ValueError("inserted thread ID is None")
+
+    post = Post(
+        thread_id=thread.id, reply_to=None, content="Porche 911", author_id=user.id
+    )
+    try:
+        post = models.posts.insert(post)
+    except Exception as e:
+        raise Exception(f"error upon inserting post: {e}")
+    if post is None:
+        raise ValueError("no post returned upon insertion")
+    if post.id is None:
+        raise ValueError("inserted post ID is None")
+
+    try:
+        post = models.posts.restore(post.id)
+    except Exception as e:
+        raise Exception(f"error upon restoring post: {e}")
+    if post is None:
+        raise ValueError("no post returned upon restoring")
+    if post.deleted is None:
+        raise ValueError("inserted post deleted status is None")
+    assert post.deleted is False
