d6c27de5eadcb93cf75e26952251426b1200ce70
r3d5un <r3d5un@pm.me>
2025-04-20 12:16:57 +0200

    Implement a PostModel select method

--- Python/src/python/db/post.py
+++ Python/src/python/db/post.py
@@ -33,6 +33,50 @@ class PostModel:
     def __init__(self, engine: Engine):
         self.engine = engine
 
+    def select(self, id: uuid.UUID) -> Post | None:
+        query = text(
+            """
+            SELECT id,
+                   thread_id,
+                   reply_to,
+                   author_id,
+                   content,
+                   created_at,
+                   updated_at,
+                   likes,
+                   deleted,
+                   deleted_at
+            FROM forum.posts
+            WHERE id = :id;
+            """
+        )
+
+        session = sessionmaker(bind=self.engine)()
+        with session:
+            try:
+                row = session.execute(
+                    query,
+                    {
+                        "id": id,
+                    },
+                ).first()
+                if row is None:
+                    raise NoResultFound
+                session.commit()
+                return Post(
+                    id=row.id,
+                    thread_id=row.thread_id,
+                    reply_to=row.reply_to,
+                    author_id=row.author_id,
+                    content=row.content,
+                    created_at=row.created_at,
+                    updated_at=row.updated_at,
+                    deleted=row.deleted,
+                    deleted_at=row.deleted_at,
+                )
+            except Exception as e:
+                raise e
+
     def insert(self, post: Post) -> Post | None:
         query = text(
             """
