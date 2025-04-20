0f3e0222b5d1c3284f1397828afbc413118a20ce
r3d5un <r3d5un@pm.me>
2025-04-20 12:12:52 +0200

    Implement a PostModel insert method

--- Python/src/python/db/post.py
+++ Python/src/python/db/post.py
@@ -2,18 +2,20 @@ import uuid
 from datetime import datetime
 from typing import Optional
 
-from sqlalchemy import Engine
+from sqlalchemy import Engine, text
+from sqlalchemy.exc import NoResultFound
+from sqlalchemy.orm import sessionmaker
 from sqlmodel import Field, SQLModel
 
 
 class Post(SQLModel, table=True):
-    __tablename__ = "forums"  # type: ignore
+    __tablename__ = "posts"  # type: ignore
     __table_args__ = {"schema": "forum"}
 
     id: Optional[uuid.UUID] = Field(default_factory=uuid.uuid4, primary_key=True)
     thread_id: uuid.UUID = Field()
     reply_to: Optional[uuid.UUID] = Field(default=None)
-    authro_id: uuid.UUID = Field()
+    author_id: uuid.UUID = Field()
     content: str = Field(nullable=False)
     created_at: datetime = Field(nullable=False, default_factory=datetime.now)
     updated_at: datetime = Field(nullable=False, default_factory=datetime.now)
@@ -21,10 +23,7 @@ class Post(SQLModel, table=True):
     deleted_at: Optional[datetime] = Field(nullable=False, default_factory=datetime.now)
 
 
-class PostPatch(SQLModel, table=True):
-    __tablename__ = "forums"  # type: ignore
-    __table_args__ = {"schema": "forum"}
-
+class PostPatch(SQLModel):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     thread_id: Optional[uuid.UUID] = Field(default=None, nullable=False)
     content: Optional[str] = Field(default=None, nullable=False)
@@ -33,3 +32,53 @@ class PostPatch(SQLModel, table=True):
 class PostModel:
     def __init__(self, engine: Engine):
         self.engine = engine
+
+    def insert(self, post: Post) -> Post | None:
+        query = text(
+            """
+            INSERT INTO forum.posts(thread_id, reply_to, content, author_id)
+            VALUES (:thread_id,
+                    :reply_to,
+                    :content,
+                    :author_id)
+            RETURNING id,
+                thread_id,
+                reply_to,
+                author_id,
+                content,
+                created_at,
+                updated_at,
+                likes,
+                deleted,
+                deleted_at;
+            """
+        )
+
+        session = sessionmaker(bind=self.engine)()
+        with session:
+            try:
+                row = session.execute(
+                    query,
+                    {
+                        "thread_id": post.thread_id,
+                        "reply_to": post.reply_to,
+                        "content": post.content,
+                        "author_id": post.author_id,
+                    },
+                ).first()
+                if row is None:
+                    raise NoResultFound
+                session.commit()
+                return Post(
+                    id=row.id,
+                    thread_id=row.thread_id,
+                    reply_to=row.reply_to,
+                    author_id=row.author_id,
+                    content=row.content,
+                    created_at=row.created_at,
+                    updated_at=row.updated_at,
+                    deleted=row.deleted,
+                    deleted_at=row.deleted_at,
+                )
+            except Exception as e:
+                raise e
