d1786092c64bdcb70ae4309bf0caa42775635285
r3d5un <r3d5un@pm.me>
2025-04-26 14:14:48 +0200

    Add helper functions for translating database types to Go types

--- Go/internal/database/database.go
+++ Go/internal/database/database.go
@@ -2,6 +2,8 @@ package database
 
 import (
 	"context"
+	"database/sql"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -24,4 +26,90 @@ func OpenPool(ctx context.Context, config DatabaseConfig) (*pgxpool.Pool, error)
 	}
 
 	return pool, nil
+}
+
+func NewNullString(s *string) sql.NullString {
+	if s == nil {
+		return sql.NullString{Valid: false}
+	}
+	return sql.NullString{String: *s, Valid: true}
+}
+
+func NewNullBool(b *bool) sql.NullBool {
+	if b == nil {
+		return sql.NullBool{Valid: false}
+	}
+	return sql.NullBool{Bool: *b, Valid: true}
+}
+
+func NewNullTime(t *time.Time) sql.NullTime {
+	if t == nil {
+		return sql.NullTime{Valid: false}
+	}
+	return sql.NullTime{Time: *t, Valid: true}
+}
+
+func NewNullInt32(i *int) sql.NullInt32 {
+	if i == nil {
+		return sql.NullInt32{Valid: false}
+	}
+	return sql.NullInt32{Int32: int32(*i), Valid: true}
+}
+
+func NewNullInt64(i *int) sql.NullInt64 {
+	if i == nil {
+		return sql.NullInt64{Valid: false}
+	}
+	return sql.NullInt64{Int64: int64(*i), Valid: true}
+}
+
+func NewNullFloat64(f *float64) sql.NullFloat64 {
+	if f == nil {
+		return sql.NullFloat64{Valid: false}
+	}
+	return sql.NullFloat64{Float64: float64(*f), Valid: true}
+}
+
+func NullStringToPtr(ns sql.NullString) *string {
+	if !ns.Valid {
+		return nil
+	}
+	return &ns.String
+}
+
+func NullBoolToPtr(nb sql.NullBool) *bool {
+	if !nb.Valid {
+		return nil
+	}
+	return &nb.Bool
+}
+
+func NullTimeToPtr(nt sql.NullTime) *time.Time {
+	if !nt.Valid {
+		return nil
+	}
+	return &nt.Time
+}
+
+func NullInt32ToPtr(ni sql.NullInt32) *int {
+	if !ni.Valid {
+		return nil
+	}
+	val := int(ni.Int32)
+	return &val
+}
+
+func NullInt64ToPtr(ni sql.NullInt64) *int {
+	if !ni.Valid {
+		return nil
+	}
+	val := int(ni.Int64)
+	return &val
+}
+
+func NullFloat64ToPtr(nf sql.NullFloat64) *float64 {
+	if !nf.Valid {
+		return nil
+	}
+	return &nf.Float64
 }
