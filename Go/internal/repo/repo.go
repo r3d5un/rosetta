e2b171f874bafa8b5801b97fe925590d0cc3ab1b
r3d5un <r3d5un@pm.me>
2025-05-04 11:33:18 +0200

    Add ForumWriter and ForumReader to package repository

--- Go/internal/repo/repo.go
+++ Go/internal/repo/repo.go
@@ -3,9 +3,17 @@ package repo
 import "github.com/r3d5un/rosetta/Go/internal/data"
 
 type Repository struct {
-	models *data.Models
+	models      *data.Models
+	ForumReader ForumReader
+	ForumWriter ForumWriter
 }
 
 func NewRepository(models *data.Models) Repository {
-	return Repository{models: models}
+	forumRepo := NewForumRepository(models)
+
+	return Repository{
+		models:      models,
+		ForumReader: &forumRepo,
+		ForumWriter: &forumRepo,
+	}
 }
