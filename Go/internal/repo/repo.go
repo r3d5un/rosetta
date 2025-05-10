fc28b7006fd55d139d5783308cb246f98817536a
r3d5un <r3d5un@pm.me>
2025-05-10 17:15:49 +0200

    Add thread reader and writer to repository

--- Go/internal/repo/repo.go
+++ Go/internal/repo/repo.go
@@ -3,18 +3,23 @@ package repo
 import "github.com/r3d5un/rosetta/Go/internal/data"
 
 type Repository struct {
-	models      *data.Models
-	ForumReader ForumReader
-	ForumWriter ForumWriter
+	models       *data.Models
+	ForumReader  ForumReader
+	ForumWriter  ForumWriter
+	ThreadReader ThreadWriter
+	ThreadWriter ThreadWriter
 }
 
 func NewRepository(models *data.Models) Repository {
 	userRepo := NewUserRepository(models)
 	forumRepo := NewForumRepository(models, &userRepo)
+	threadRepo := NewThreadRepository(models, &forumRepo, &userRepo)
 
 	return Repository{
-		models:      models,
-		ForumReader: &forumRepo,
-		ForumWriter: &forumRepo,
+		models:       models,
+		ForumReader:  &forumRepo,
+		ForumWriter:  &forumRepo,
+		ThreadReader: &threadRepo,
+		ThreadWriter: &threadRepo,
 	}
 }
