9c237a0561814baab8dbe421e5df12b23f7c54ec
r3d5un <r3d5un@pm.me>
2025-05-18 02:46:45 +0200

    Test the ForumRepository Create method

---
+++ Go/internal/repo/forums_test.go
@@ -0,0 +1,53 @@
+package repo_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/r3d5un/rosetta/Go/internal/repo"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestForumRepository(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	user := repo.User{
+		Name:     "Saburo Arasaka",
+		Username: "s.arasaka",
+		Email:    "s.arasaka@arasaka.com",
+	}
+	u, err := repository.UserWriter.Create(ctx, user)
+	assert.NoError(t, err)
+
+	forum := repo.Forum{
+		OwnerID: u.ID,
+		Name:    "Crushing Militech",
+	}
+
+	t.Run("Create", func(t *testing.T) {
+		f, err := repository.ForumWriter.Create(ctx, forum)
+		assert.NoError(t, err)
+
+		forum = *f
+	})
+
+	t.Run("Read", func(t *testing.T) {
+	})
+
+	t.Run("List", func(t *testing.T) {
+	})
+
+	t.Run("Update", func(t *testing.T) {
+	})
+
+	t.Run("Delete", func(t *testing.T) {
+	})
+
+	t.Run("Restore", func(t *testing.T) {
+	})
+
+	t.Run("PermanentlyDelete", func(t *testing.T) {
+	})
+}
