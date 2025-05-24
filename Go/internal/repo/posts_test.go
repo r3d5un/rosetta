505922673d25c7d9a2bfa7d56a60bd9c0ed59bba
r3d5un <r3d5un@pm.me>
2025-05-24 13:35:15 +0200

    Test the PostRepository Create method

---
+++ Go/internal/repo/posts_test.go
@@ -0,0 +1,69 @@
+package repo_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/r3d5un/rosetta/Go/internal/repo"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPostRepository(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	user := repo.User{
+		Name:     "delemain",
+		Username: "delamain",
+		Email:    "delamain@delamain.com",
+	}
+	u, err := repository.UserWriter.Create(ctx, user)
+	assert.NoError(t, err)
+
+	forum := repo.Forum{
+		OwnerID: u.ID,
+		Name:    "Troublesome taxis",
+	}
+	f, err := repository.ForumWriter.Create(ctx, forum)
+	assert.NoError(t, err)
+
+	thread := repo.Thread{
+		AuthorID: u.ID,
+		ForumID:  f.ID,
+		Title:    "Rouge cars",
+	}
+	newThread, err := repository.ThreadWriter.Create(ctx, thread)
+	assert.NoError(t, err)
+
+	post := repo.Post{
+		ThreadID: newThread.ID,
+		Content:  "A rogue taxi is nearby, here are the precise coordinates",
+		AuthorID: u.ID,
+	}
+
+	t.Run("Create", func(t *testing.T) {
+		p, err := repository.PostWriter.Create(ctx, post)
+		assert.NoError(t, err)
+
+		post = *p
+	})
+
+	t.Run("Read", func(t *testing.T) {
+	})
+
+	t.Run("List", func(t *testing.T) {
+	})
+
+	t.Run("Update", func(t *testing.T) {
+	})
+
+	t.Run("Delete", func(t *testing.T) {
+	})
+
+	t.Run("Restore", func(t *testing.T) {
+	})
+
+	t.Run("PermanentlyDelete", func(t *testing.T) {
+	})
+}
