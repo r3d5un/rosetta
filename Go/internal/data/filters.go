2ccb772bd35be51784ebbd75d46e4607926c90d9
r3d5un <r3d5un@pm.me>
2025-05-24 13:05:46 +0200

    Omit zero values from filter JSON output

--- Go/internal/data/filters.go
+++ Go/internal/data/filters.go
@@ -13,28 +13,28 @@ type Metadata struct {
 }
 
 type Filters struct {
-	ID            *uuid.UUID `json:"id"`
-	OwnerID       *uuid.UUID `json:"ownerId"`
-	UserID        *uuid.UUID `json:"userId"`
-	PostID        *uuid.UUID `json:"postId"`
-	ThreadID      *uuid.UUID `json:"threadId"`
-	ForumID       *uuid.UUID `json:"forumId"`
-	AuthorID      *uuid.UUID `json:"authorId"`
-	Name          *string    `json:"name"`
-	Title         *string    `json:"title"`
-	Username      *string    `json:"username,omitempty"`
-	Email         *string    `json:"email,omitempty"`
-	CreatedAtFrom *time.Time `json:"createdAtFrom"`
-	CreatedAtTo   *time.Time `json:"createdAtTo"`
-	UpdatedAtFrom *time.Time `json:"updatedAtFrom"`
-	UpdatedAtTo   *time.Time `json:"updatedAtTo"`
-	DeletedAtFrom *time.Time `json:"deletedAtFrom"`
-	DeletedAtTo   *time.Time `json:"deletedAtTo"`
-	Deleted       *bool      `json:"deleted"`
-	IsLocked      *bool      `json:"isLocked"`
+	ID            *uuid.UUID `json:"id,omitzero"`
+	OwnerID       *uuid.UUID `json:"ownerId,omitzero"`
+	UserID        *uuid.UUID `json:"userId,omitzero"`
+	PostID        *uuid.UUID `json:"postId,omitzero"`
+	ThreadID      *uuid.UUID `json:"threadId,omitzero"`
+	ForumID       *uuid.UUID `json:"forumId,omitzero"`
+	AuthorID      *uuid.UUID `json:"authorId,omitzero"`
+	Name          *string    `json:"name,omitzero"`
+	Title         *string    `json:"title,omitzero"`
+	Username      *string    `json:"username,omitzero"`
+	Email         *string    `json:"email,omitzero"`
+	CreatedAtFrom *time.Time `json:"createdAtFrom,omitzero"`
+	CreatedAtTo   *time.Time `json:"createdAtTo,omitzero"`
+	UpdatedAtFrom *time.Time `json:"updatedAtFrom,omitzero"`
+	UpdatedAtTo   *time.Time `json:"updatedAtTo,omitzero"`
+	DeletedAtFrom *time.Time `json:"deletedAtFrom,omitzero"`
+	DeletedAtTo   *time.Time `json:"deletedAtTo,omitzero"`
+	Deleted       *bool      `json:"deleted,omitzero"`
+	IsLocked      *bool      `json:"isLocked,omitzero"`
 
-	OrderBy         []string  `json:"order_by,omitempty"`
-	OrderBySafeList []string  `json:"order_by_safe_list,omitempty"`
-	LastSeen        uuid.UUID `json:"lastSeen"`
-	PageSize        int       `json:"page_size,omitempty"`
+	OrderBy         []string  `json:"order_by,omitzero"`
+	OrderBySafeList []string  `json:"order_by_safe_list,omitzero"`
+	LastSeen        uuid.UUID `json:"lastSeen,omitzero"`
+	PageSize        int       `json:"page_size,omitzero"`
 }
